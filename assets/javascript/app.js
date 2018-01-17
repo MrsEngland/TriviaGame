0230002ea2d7df8bfdb1cb5d0c1d41fc98d84fcf
Donna England <mrsengland@aol.com>
2018-01-17 18:38:20 -0500

    upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+
+var questions = {
+          {
+            question: "What is the largest freshwater lake in the world?",
+            answers: {
+              a: "Lake Superior",
+              b: "Lake Michigan",
+              c: "Lake Tahoe",
+              d: "Lake Huron"
+            },
+            correctAnswer: "a"
+          },
+          {
+            question: "Where would you find the Sea of Tranquility",
+            answers: {
+              a: "Puerto Rico",
+              b: "Mars",
+              c: "The Moon", 
+              d: "The British Virgin Islands"
+           },
+            correctAnswer: "c"
+          }
+        };
+  
+  
+  console.log(questions.question.answers[0]);
+  console.log(questions.question.answers[1]);
+
+  
+
+
+
+
+
